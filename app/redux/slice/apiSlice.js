73d6b070ab9893b2b30909a290a7d9741c1a5e30
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-05-07 17:09:24 +0530

    5432

--- app/redux/slice/apiSlice.js
+++ app/redux/slice/apiSlice.js
@@ -3,19 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
   reducerPath: "Api",
   baseQuery:
-    fetchBaseQuery({
-      baseUrl: "http://192.168.1.11:7190/api"
-    }),
-  // fetchBaseQuery({
-  //   baseUrl: "http://192.168.63.195:7190/api",
-  // }),
+    // fetchBaseQuery({
+    //   baseUrl: "http://192.168.1.11:7190/api"
+    // }),
+    // fetchBaseQuery({
+    //   baseUrl: "http://192.168.63.195:7190/api",
+    // }),
 
-  // fetchBaseQuery({
-  // 	baseUrl: "http://192.168.84.86:7190/api"
-  // }),
-  // fetchBaseQuery({
-  // 	baseUrl: "http://192.168.29.225:7190/api"
-  // }),
-  // fetchBaseQuery({ baseUrl: "https://work.grovyo.xyz/api" }),
+    // fetchBaseQuery({
+    // 	baseUrl: "http://192.168.84.86:7190/api"
+    // }),
+    // fetchBaseQuery({
+    // 	baseUrl: "http://192.168.29.225:7190/api"
+    // }),
+    fetchBaseQuery({ baseUrl: "https://work.grovyo.xyz/api" }),
   endpoints: () => ({}),
 });
